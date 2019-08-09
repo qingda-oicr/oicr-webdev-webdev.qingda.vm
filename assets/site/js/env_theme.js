992c5a5965409826a319ff3414e3c21ccdf3093d
Yifei Zhang <yifei.zhang@oicr.on.ca>
2019-08-09 14:37:22 -0400

    updated env_theme with correct docker port

--- assets/site/js/env_theme.js
+++ assets/site/js/env_theme.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
     APP_ID: 'webdevff0b3218aaad11e7abc4cec278b6b50a_APP_ID',
     API_HOST:
-        'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_DOCKER_PORT',
+        'webdevff0b3218aaad11e7abc4cec278b6b50a_API_HOST:webdevff0b3218aaad11e7abc4cec278b6b50a_API_PORT',
     CONTACT_LINK: 'webdevff0b3218aaad11e7abc4cec278b6b50a_MAIN_CONTACT',
     CMUI_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_CMUI_ENABLED,
     FORM_ENABLED: webdevff0b3218aaad11e7abc4cec278b6b50a_FORM_ENABLED,
